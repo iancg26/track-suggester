55b357f83f5317e6410e32f949592b6c38881c85
Ian Garner <icgarner21@gmail.com>
2017-04-28 11:34:40 -0700

    add js else if statements

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+  $("form#tracksuggester").submit(function(event) {
+
+    var name = $("select#name").val();
+    var system = $("select#system").val();
+    var app = $("select#app").val();
+    var role = $("select#role").val();
+    var details = $("select#details").val();
+    var style = $("select#style").val();
+
+      if (system = "windows" ) {
+
+        $("#course").empty().append("C#");
+      } else if (system === "macos") {
+        $("#course").empty().append("CSS");
+      } else if (system === "linux") {
+        $("#course").empty().append("PHP");
+      } else {
+        $("#course").empty().append("Java");
+      }
+      if (app === "tinder") {
+          $("#course").empty().append("CSS");
+      } else if (app === "facebook") {
+        $("#course").empty().append("C#");
+      } else if (app === "snapchat") {
+        $("#course").empty().append("PHP");
+      } else {
+        $("#course").empty().append("Java");
+      }
+      if (role === "wallflower") {
+        $("#course").empty().append("PHP");
+      } else if (role === "leader") {
+        $("#course").empty().append("C#");
+      } else if (role === "follower") {
+        $("#course").empty().append("CSS");
+      } else {
+        $("#course").empty().append("Java");
+      }
+
+
+    $("#statement").show();
+
+    event.preventDefault();
+  });
+});
